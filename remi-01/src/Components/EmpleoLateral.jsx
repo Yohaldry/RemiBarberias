5fca9a8906d20e8a1df99cf03d0d4088c8f03061
Yohaldry <yohaldryquintero1995@gmail.com>
2021-10-15 04:49:38 -0500

    remi

--- remi-01/src/Components/EmpleoLateral.jsx
+++ remi-01/src/Components/EmpleoLateral.jsx
@@ -16,7 +16,7 @@ const EmpleoLateral = () => {
             <img src="https://res.cloudinary.com/dtkirmtfq/image/upload/v1632818447/Remi/Imagen3_p5ft9s.png" alt="" />
             <hr />
             <ul>
-                <input id="BuscarEmpleo" type="text" placeholder="Buscar"/>
+                
                 <a href="/"><li>Soporte</li></a>
                 <Link to="/"><li>Salir</li></Link>
             </ul>
