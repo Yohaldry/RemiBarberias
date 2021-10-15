de5f761bfb87674fbe49b830f74c3f4b48adee40
DavidCardonaG <softwarewd@outlook.com>
2021-10-15 04:47:47 -0500

    perfil

--- remi-01/src/Components/Perfil.jsx
+++ remi-01/src/Components/Perfil.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
 
-const Perfil = () => {
+const Perfil = () => { 
+    const {name,email,image } = useSelector((store) => store.login); 
 
-    
+
 
     return (
         <div className="About_Perfil">
             <h1>Perfil</h1>
         <div className="About_perfilFoto">
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+            <img src={image} alt="" />
          <div className="Caracteristicas_Perfil">
-                 <h1>Yohaldry Quintero</h1>
+                 <h1>{name}</h1>
+                 <h2>{email}</h2>
                  <h3>Direccion: Carrera 57a #5b-18</h3>
            </div>
         
